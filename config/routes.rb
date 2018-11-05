8bc959d8079c46192bfc792ed77f1dcff756abad
kazukinakamura <kajirikajiri@gmail.com>
2018-11-05 11:03:20 +0000

    dificult

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   get 'static_pages/result'
   post 'static_pages/result'
   get 'static_pages/home'
-  root 'static_pages#home'
+  # root 'static_pages#home'
+  root to: 'home#index'
 end
