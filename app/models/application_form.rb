79ab89de06d22378ada30ab01f3485dfad27fb04
kazukinakamura <kajirikajiri@gmail.com>
2018-11-10 05:35:32 +0000

    out validates_inclusion_of in greater_than & less_than

--- app/models/application_form.rb
+++ app/models/application_form.rb
@@ -2,12 +2,12 @@ class ApplicationForm
   include ActiveModel::Model
 
   attr_accessor :height, :weight, :old
-
-  validates :height , :numericality => true, presence: true
-  validates :weight , :numericality => true, presence: true
-  validates :old   , :numericality => true, presence: true
-  validates_inclusion_of :old, in: 0..500
-  validates_inclusion_of :weight, in: 0..500
-  validates_inclusion_of :height, in: 0..500
-
+  
+  validates :height, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 500 }
+  validates :weight, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 500 }
+  validates :old   , presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 500 }
+  
+  # validates_inclusion_of :height, in: 0..500
+  # validates_inclusion_of :weight, in: 0..500
+  # validates_inclusion_of :old   , in: 0..500
 end
