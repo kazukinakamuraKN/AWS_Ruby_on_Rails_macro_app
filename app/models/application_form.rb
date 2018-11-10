edd4d25f637d2a693308e919f6a295cd19ead1de
kazukinakamura <kajirikajiri@gmail.com>
2018-11-10 10:42:28 +0000

    save

--- app/models/application_form.rb
+++ app/models/application_form.rb
@@ -1,13 +1,8 @@
 class ApplicationForm
   include ActiveModel::Model
 
-  attr_accessor :height, :weight, :old
+  attr_accessor :height, :weight, :old#, :gender, :activity, :purpose
   
   validates :height, :weight, :old, numericality: { only_integer: true, greater_than: 0, less_than: 500 }
-  # validates :weight, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 500 }
-  # validates :old   , presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 500 }
-  
-  # validates_inclusion_of :height, in: 0..500
-  # validates_inclusion_of :weight, in: 0..500
-  # validates_inclusion_of :old   , in: 0..500
+
 end
--- test/integration/result_test.rb
+++ test/integration/result_test.rb
@@ -5,40 +5,67 @@ class ResultTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   test "valid root_path test" do
+    get root_path
+    assert_template 'static_pages/home'
+  end
+  
+  test "valid home_path test" do
     get home_path
     assert_template 'static_pages/home'
   end
   
   test "valid result test" do
     get root_path
-    post result_path, params: { page: { gender:   "male",
-                                        activity: "low",
-                                        purpose:  "loss"},
-                                height: 123,
-                                weight: 123,
-                                old:    123}
+    post result_path, params: { gender:   "male",
+                                activity: "low",
+                                purpose:  "loss",
+                                height:   123,
+                                weight:   123,
+                                old:      123}
     assert_template 'static_pages/result'
   end
   
   test "invalid result test" do
     get root_path
-    post result_path, params: { page: { gender:   "",
-                                        activity: "",
-                                        purpose:  ""},
-                                height: 1234,
-                                weight: 1234,
-                                old:    1234}
+    post result_path, params: { gender:   "",
+                                activity: "",
+                                purpose:  "",
+                                height:   "",
+                                weight:   "",
+                                old:      ""}
     assert_template 'static_pages/home'
   end
   
-  test "invalid numericality test" do
+  test "valid numericality option test" do
     get root_path
-    post result_path, params: { page: { gender:   "male",
-                                        activity: "low",
-                                        purpose:  "loss"},
-                                height: "a",
-                                weight: "a",
-                                old:    "a"}
+    post result_path, params: { gender:   "male",
+                                activity: "low",
+                                purpose:  "loss",
+                                height:   1234,
+                                weight:   1234,
+                                old:      1234}
+    assert_template 'static_pages/home'
+  end
+  
+  test "valid numericality test" do
+    get root_path
+    post result_path, params: { gender:   "male",
+                                activity: "low",
+                                purpose:  "loss",
+                                height:   "a",
+                                weight:   "a",
+                                old:      "a"}
+    assert_template 'static_pages/home'
+  end
+  
+    test "valid blank test" do
+    get root_path
+    post result_path, params: { gender:   "male",
+                                activity: "low",
+                                purpose:  "loss",
+                                height:   "",
+                                weight:   "",
+                                old:      ""}
     assert_template 'static_pages/home'
   end
   
